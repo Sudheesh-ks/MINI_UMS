1b5827e16ccc480a26f385a36bca98dc84c6317e
Sudheesh ks <sudheeshks132@gmail.com>
2026-05-20 17:02:01 +0530

    feat: logout feature implemented

--- frontend/src/pages/user/HomePage.jsx
+++ frontend/src/pages/user/HomePage.jsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("usertoken");
+    navigate('/');
+  }
+  
   return (
     <div>
-      Home
+      <h1>Welcome Home</h1>
+      <button onClick={logout}>Logout</button>
     </div>
   )
 }
